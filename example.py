094c7325bf8fc9d200d64ef02571d12cef8e6bcc
Stefano <stefano.forti92@gmail.com>
2018-03-01 09:47:49 +0100

    new example

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from fog_director import *
 fd = FogDirSim()
 
 fd.add_thing("fire0", "fire")
-fd.add_thing("temperature0", "temperature")
+fd.add_thing("thermostate0", "thermostate")
 fd.add_thing("water0", "water")
 fd.add_thing("video0", "video")
 
@@ -25,28 +25,29 @@ fog_2 = Node("fog_2", hw2, ["linux", "php"])
 fd.add_node(fog_2)
 
 #fog3
-hdd3 = ProbabilityDistribution([0.8, 0.10, 0.05, 0.05],[128.0, 100.0, 70.0, 50.0])
-ram3 = ProbabilityDistribution([0.8, 0.10, 0.05, 0.05],[12.0, 10.0, 8.0, 6.0])
-cpu3 = ProbabilityDistribution([0.8, 0.2],[4.0, 2.0])
+cpu3 = ProbabilityDistribution([0.5, 0.3, 0.2],[4.0, 3.0, 2.0])
+hdd3 = ProbabilityDistribution([1.0],[50.0])
+ram3 = ProbabilityDistribution([0.8, 0.10, 0.10],[4.0, 3.0, 2.0])
+
 hw3 = HardwareResources(ram3, hdd3, cpu3)
 fog_3 = Node("fog_3", hw3, ["linux", "sql"])
 fd.add_node(fog_3)
 
-#cloud1
-hdd_c1 = ProbabilityDistribution([1.0],[160])
-ram_c1 = ProbabilityDistribution([1.0],[16])
-cpu_c1 = ProbabilityDistribution([1.0],[8])
-hw_c1 = HardwareResources(ram_c1, hdd_c1, cpu_c1)
-cloud_1 = Node("cloud_1", hw_c1, ["linux", "sql"])
-fd.add_node(cloud_1)
+# #cloud1
+# hdd_c1 = ProbabilityDistribution([1.0],[160])
+# ram_c1 = ProbabilityDistribution([1.0],[16])
+# cpu_c1 = ProbabilityDistribution([1.0],[8])
+# hw_c1 = HardwareResources(ram_c1, hdd_c1, cpu_c1)
+# cloud_1 = Node("cloud_1", hw_c1, ["linux", "sql"])
+# fd.add_node(cloud_1)
 
-#cloud2
-hdd_c2 = ProbabilityDistribution([1.0],[160])
-ram_c2 = ProbabilityDistribution([1.0],[16])
-cpu_c2 = ProbabilityDistribution([1.0],[8])
-hw_c2 = HardwareResources(ram_c2, hdd_c2, cpu_c2)
-cloud_2 = Node("cloud_1", hw_c2, ["linux", "sql"])
-fd.add_node(cloud_2)
+# #cloud2
+# hdd_c2 = ProbabilityDistribution([1.0],[160])
+# ram_c2 = ProbabilityDistribution([1.0],[16])
+# cpu_c2 = ProbabilityDistribution([1.0],[8])
+# hw_c2 = HardwareResources(ram_c2, hdd_c2, cpu_c2)
+# cloud_2 = Node("cloud_1", hw_c2, ["linux", "sql"])
+# fd.add_node(cloud_2)
 
 #fog_1 - fog_2
 b_f1_f2 = ProbabilityDistribution([0.9, 0.1], [32.0, 16.0])
@@ -58,9 +59,9 @@ q_f1_f2.sample_qos()
 fd.add_link(Link("fog_1", "fog_2", q_f1_f2))
 
 #fog_2 - fog_3
-b_f2_f3 = ProbabilityDistribution([0.9, 0.1], [32.0, 16.0])
-b_f3_f2 = ProbabilityDistribution([0.9, 0.1], [32.0, 16.0])
-l_f2_f3 = ProbabilityDistribution([1.0], [15.0])
+b_f2_f3 = ProbabilityDistribution([0.8, 0.2], [6, 2.0])
+b_f3_f2 = ProbabilityDistribution([0.8, 0.2], [60.0, 35.0])
+l_f2_f3 = ProbabilityDistribution([1.0], [60.0])
 q_f2_f3 = QoSProfile(b_f2_f3, b_f3_f2, l_f2_f3)
 q_f2_f3.sample_qos()
 
@@ -82,9 +83,9 @@ fd.add_link(Link("fog_1", "fog_3", q_f1_f3))
 # l_f1_f3 = ProbabilityDistribution([1.0], [15.0])
 
 # fd.add_link(Link("fog_1", "fire0", q) )
-# fd.add_link(Link("fog_1", "temperature0", q) )
+# fd.add_link(Link("fog_1", "thermostate0", q) )
 # fd.add_link(Link("fog_2", "fire0", q) )
-# fd.add_link(Link("fog_2", "temperature0", q) )
+# fd.add_link(Link("fog_2", "thermostate0", q) )
 
 print(fd.infrastructure.links)
 
@@ -99,7 +100,7 @@ print(app)
 fd.publish_app("app1", app)
 fd.new_deployment("dep1", "app1")
 fd.deploy_component("dep1", "SmartBuilding", "fog_1")
-fd.bind_thing("dep1", 0, "temperature0")  
+fd.bind_thing("dep1", 0, "thermostate0")  
 fd.bind_thing("dep1", 1, "fire0") 
 fd.bind_thing("dep1", 2, "video0") 
 fd.start_app("dep1")
